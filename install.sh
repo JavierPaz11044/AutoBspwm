7b81d3978173db0a3303eaa6a3dec499c878d79b
Luis Paz <luis.paz@formos.com>
2026-05-15 16:51:57 -0500

    Update install.sh to enable installation of polybar and picom dependencies

--- install.sh
+++ install.sh
@@ -28,11 +28,11 @@ fi
 
 # # Instalando Requerimientos para la polybar
 
-# sudo apt install -y polybar cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
+sudo apt install -y polybar cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
 
 # # Dependencias de Picom
 
-# sudo apt install -y meson ninja-build picom libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
+sudo apt install -y meson ninja-build picom libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
 
 # # Instalamos paquetes adionales
 
@@ -89,31 +89,31 @@ echo "export PATH=$PATH_SCRIPTS:\$PATH" >> $HOME/"$bash_file"
 # Creando carpeta de Reposistorios
 
 
-# mkdir ~/github
+ mkdir ~/github
 
 # # Descargar Repositorios Necesarios
 
-# cd ~/github
-# git clone --recursive https://github.com/polybar/polybar
-# git clone https://github.com/ibhagwan/picom.git
+cd ~/github
+git clone --recursive https://github.com/polybar/polybar
+git clone https://github.com/ibhagwan/picom.git
 
-# # Instalando Polybar
+# Instalando Polybar
 
-# cd ~/github/polybar
-# mkdir build
-# cd build
-# cmake ..
-# make -j$(nproc)
-# sudo make install
+cd ~/github/polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
 
-# # Instalando Picom (meson + ninja-build must be on PATH; ninja is from package ninja-build)
+# Instalando Picom (meson + ninja-build must be on PATH; ninja is from package ninja-build)
 
-# cd ~/github/picom
-# git submodule update --init --recursive
-# sudo apt install -y meson ninja-build
-# meson --buildtype=release . build
-# ninja -C build
-# sudo ninja -C build install
+cd ~/github/picom
+git submodule update --init --recursive
+sudo apt install -y meson ninja-build
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
 
 # # Instalando p10k
 
